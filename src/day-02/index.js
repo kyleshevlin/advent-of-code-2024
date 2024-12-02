4e540a0aafafea5b0afa2d936bced2b8778470b3
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2024-12-02 09:08:02 -0800

    solutions for day 2

--- src/day-02/index.js
+++ src/day-02/index.js
@@ -2,8 +2,56 @@ import { getInput } from '../utils'
 
 const data = getInput(__dirname)
 
-const formatInput = input => input.trim().split('\n')
+const formatInput = input =>
+  input
+    .trim()
+    .split('\n')
+    .map(line => line.split(' ').map(Number))
 
-export function solution1(input) {}
+export function isDecreasingCorrectly(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    const diff = arr[i] - arr[i + 1]
 
-export function solution2(input) {}
+    if (diff <= 0 || diff >= 4) return false
+  }
+
+  return true
+}
+
+export function isIncreasingCorrectly(arr) {
+  return isDecreasingCorrectly(arr.toReversed())
+}
+
+export function isRowSafe(arr) {
+  return isDecreasingCorrectly(arr) || isIncreasingCorrectly(arr)
+}
+
+export function solution1(input) {
+  const rows = formatInput(input)
+
+  return rows.filter(isRowSafe).length
+}
+
+// console.log(solution1(data)) // 202
+
+export function getRowPermutations(arr) {
+  const result = []
+
+  for (let i = 0; i < arr.length; i++) {
+    const permutation = [...arr.slice(0, i), ...arr.slice(i + 1)]
+
+    result.push(permutation)
+  }
+
+  return result
+}
+
+export function solution2(input) {
+  const rows = formatInput(input)
+
+  return rows.filter(
+    row => isRowSafe(row) || getRowPermutations(row).some(isRowSafe)
+  ).length
+}
+
+// console.log(solution2(data)) // 271
--- src/day-02/index.test.js
+++ src/day-02/index.test.js
@@ -1,11 +1,61 @@
-import { solution1, solution2 } from './'
+import {
+  getRowPermutations,
+  isDecreasingCorrectly,
+  isIncreasingCorrectly,
+  isRowSafe,
+  solution1,
+  solution2,
+} from './'
 
-const input = ``
+const input = `
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+`
 
 test('solution1', () => {
-  expect(solution1(input)).toEqual()
+  expect(solution1(input)).toEqual(2)
 })
 
 test('solution2', () => {
-  expect(solution2(input)).toEqual()
+  expect(solution2(input)).toEqual(4)
+})
+
+test('isRowSafe', () => {
+  expect(isRowSafe([1, 2, 3, 4, 5])).toEqual(true)
+  expect(isRowSafe([5, 4, 3, 2, 1])).toEqual(true)
+  expect(isRowSafe([1, 2, 3, 2, 1])).toEqual(false)
+  expect(isRowSafe([5, 4, 3, 4, 5])).toEqual(false)
+  expect(isRowSafe([1, 4, 7, 10, 13])).toEqual(true)
+  expect(isRowSafe([1, 5, 9, 13, 17])).toEqual(false)
+  expect(isRowSafe([1, 2, 2, 3, 4])).toEqual(false)
+  expect(isRowSafe([13, 10, 7, 4, 1])).toEqual(true)
+  expect(isRowSafe([17, 13, 9, 5, 1])).toEqual(false)
+})
+
+test('isDecreasingCorrectly', () => {
+  expect(isDecreasingCorrectly([1, 1, 1])).toEqual(false)
+  expect(isDecreasingCorrectly([3, 2, 1])).toEqual(true)
+  expect(isDecreasingCorrectly([9, 5, 1])).toEqual(false)
+  expect(isDecreasingCorrectly([3, 2, 3])).toEqual(false)
+})
+
+test('isIncreasingCorrectly', () => {
+  expect(isIncreasingCorrectly([1, 1, 1])).toEqual(false)
+  expect(isIncreasingCorrectly([1, 2, 3])).toEqual(true)
+  expect(isIncreasingCorrectly([1, 5, 9])).toEqual(false)
+  expect(isIncreasingCorrectly([1, 2, 1])).toEqual(false)
+})
+
+test('getRowPermutations', () => {
+  expect(getRowPermutations([1, 2, 3, 4, 5])).toEqual([
+    [2, 3, 4, 5],
+    [1, 3, 4, 5],
+    [1, 2, 4, 5],
+    [1, 2, 3, 5],
+    [1, 2, 3, 4],
+  ])
 })
