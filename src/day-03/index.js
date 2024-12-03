0810746c90929e753ae59ed5f39982c4359f39ae
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2024-12-02 22:40:51 -0800

    day 3 solution

--- src/day-03/index.js
+++ src/day-03/index.js
@@ -1,9 +1,59 @@
-import { getInput } from '../utils'
+import { add, getInput } from '../utils'
 
 const data = getInput(__dirname)
 
 const formatInput = input => input.trim().split('\n')
 
-export function solution1(input) {}
+const MUL_PATTERN = /mul\(\d{1,3},\d{1,3}\)/g
 
-export function solution2(input) {}
+function sumMuls(muls) {
+  return muls
+    .map(match => {
+      const [x, y] = match
+        .replace('mul(', '')
+        .replace(')', '')
+        .split(',')
+        .map(Number)
+
+      return x * y
+    })
+    .reduce(add, 0)
+}
+
+export function solution1(input) {
+  const lines = formatInput(input)
+  const matches = lines.flatMap(line => line.match(MUL_PATTERN))
+
+  return sumMuls(matches)
+}
+
+// console.log(solution1(data)) // 170778545
+
+const MUL_DO_DONT_PATTERN = /mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)/g
+
+export function solution2(input) {
+  const lines = formatInput(input)
+  const matches = lines.flatMap(line => line.match(MUL_DO_DONT_PATTERN))
+
+  const kept = []
+  let keeping = true
+  for (const match of matches) {
+    if (match === 'do()') {
+      keeping = true
+      continue
+    }
+
+    if (match === "don't()") {
+      keeping = false
+      continue
+    }
+
+    if (keeping) {
+      kept.push(match)
+    }
+  }
+
+  return sumMuls(kept)
+}
+
+// console.log(solution2(data)) // 82868252
--- src/day-03/index.test.js
+++ src/day-03/index.test.js
@@ -1,11 +1,17 @@
 import { solution1, solution2 } from './'
 
-const input = ``
+const input = `
+xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+`
 
 test('solution1', () => {
-  expect(solution1(input)).toEqual()
+  expect(solution1(input)).toEqual(161)
 })
 
+const input2 = `
+xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+`
+
 test('solution2', () => {
-  expect(solution2(input)).toEqual()
+  expect(solution2(input2)).toEqual(48)
 })
