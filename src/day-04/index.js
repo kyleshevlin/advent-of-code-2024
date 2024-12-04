b23ff3521c2f58eef6bdd3b07b2f9336f326f256
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2024-12-04 08:33:01 -0800

    rename vars

--- src/day-04/index.js
+++ src/day-04/index.js
@@ -9,69 +9,69 @@ export function solution1(input) {
 
   let count = 0
 
-  for (let y = 0; y < search.length; y++) {
-    for (let x = 0; x < search[y].length; x++) {
-      if (search[y][x] === 'X') {
+  for (let r = 0; r < search.length; r++) {
+    for (let c = 0; c < search[r].length; c++) {
+      if (search[r][c] === 'X') {
         // forwards
-        if (search[y].substring(x, x + 4) === 'XMAS') {
+        if (search[r].substring(c, c + 4) === 'XMAS') {
           count++
         }
 
         // backwards
-        if (search[y].substring(x - 3, x + 1) === 'SAMX') {
+        if (search[r].substring(c - 3, c + 1) === 'SAMX') {
           count++
         }
 
         // down
         if (
-          safeGridGet(search, y + 1, x) === 'M' &&
-          safeGridGet(search, y + 2, x) === 'A' &&
-          safeGridGet(search, y + 3, x) === 'S'
+          safeGridGet(search, r + 1, c) === 'M' &&
+          safeGridGet(search, r + 2, c) === 'A' &&
+          safeGridGet(search, r + 3, c) === 'S'
         ) {
           count++
         }
 
         // up
         if (
-          safeGridGet(search, y - 1, x) === 'M' &&
-          safeGridGet(search, y - 2, x) === 'A' &&
-          safeGridGet(search, y - 3, x) === 'S'
+          safeGridGet(search, r - 1, c) === 'M' &&
+          safeGridGet(search, r - 2, c) === 'A' &&
+          safeGridGet(search, r - 3, c) === 'S'
         ) {
           count++
         }
 
         // down-to-the-right
         if (
-          safeGridGet(search, y + 1, x + 1) === 'M' &&
-          safeGridGet(search, y + 2, x + 2) === 'A' &&
-          safeGridGet(search, y + 3, x + 3) === 'S'
+          safeGridGet(search, r + 1, c + 1) === 'M' &&
+          safeGridGet(search, r + 2, c + 2) === 'A' &&
+          safeGridGet(search, r + 3, c + 3) === 'S'
         ) {
           count++
         }
 
         // up-to-the-left
         if (
-          safeGridGet(search, y - 1, x - 1) === 'M' &&
-          safeGridGet(search, y - 2, x - 2) === 'A' &&
-          safeGridGet(search, y - 3, x - 3) === 'S'
+          safeGridGet(search, r - 1, c - 1) === 'M' &&
+          safeGridGet(search, r - 2, c - 2) === 'A' &&
+          safeGridGet(search, r - 3, c - 3) === 'S'
         ) {
           count++
         }
 
         // down-to-the-left
         if (
-          safeGridGet(search, y + 1, x - 1) === 'M' &&
-          safeGridGet(search, y + 2, x - 2) === 'A' &&
-          safeGridGet(search, y + 3, x - 3) === 'S'
+          safeGridGet(search, r + 1, c - 1) === 'M' &&
+          safeGridGet(search, r + 2, c - 2) === 'A' &&
+          safeGridGet(search, r + 3, c - 3) === 'S'
         ) {
           count++
         }
 
         // up-to-the-right
         if (
-          safeGridGet(search, y - 1, x + 1) === 'M' &&
-          safeGridGet(search, y - 2, x + 2) === 'A' &&
-          safeGridGet(search, y - 3, x + 3) === 'S'
+          safeGridGet(search, r - 1, c + 1) === 'M' &&
+          safeGridGet(search, r - 2, c + 2) === 'A' &&
+          safeGridGet(search, r - 3, c + 3) === 'S'
         ) {
           count++
         }
@@ -89,19 +89,19 @@ export function solution2(input) {
 
   let count = 0
 
-  for (let y = 0; y < search.length; y++) {
-    for (let x = 0; x < search[y].length; x++) {
-      if (search[y][x] === 'A') {
+  for (let r = 0; r < search.length; r++) {
+    for (let c = 0; c < search[r].length; c++) {
+      if (search[r][c] === 'A') {
         /**
          * M . M
          * . A .
          * S . S
          */
         if (
-          safeGridGet(search, y - 1, x - 1) === 'M' &&
-          safeGridGet(search, y - 1, x + 1) === 'M' &&
-          safeGridGet(search, y + 1, x - 1) === 'S' &&
-          safeGridGet(search, y + 1, x + 1) === 'S'
+          safeGridGet(search, r - 1, c - 1) === 'M' &&
+          safeGridGet(search, r - 1, c + 1) === 'M' &&
+          safeGridGet(search, r + 1, c - 1) === 'S' &&
+          safeGridGet(search, r + 1, c + 1) === 'S'
         ) {
           count++
         }
@@ -112,10 +112,10 @@ export function solution2(input) {
          * M . M
          */
         if (
-          safeGridGet(search, y - 1, x - 1) === 'S' &&
-          safeGridGet(search, y - 1, x + 1) === 'S' &&
-          safeGridGet(search, y + 1, x - 1) === 'M' &&
-          safeGridGet(search, y + 1, x + 1) === 'M'
+          safeGridGet(search, r - 1, c - 1) === 'S' &&
+          safeGridGet(search, r - 1, c + 1) === 'S' &&
+          safeGridGet(search, r + 1, c - 1) === 'M' &&
+          safeGridGet(search, r + 1, c + 1) === 'M'
         ) {
           count++
         }
@@ -126,10 +126,10 @@ export function solution2(input) {
          * S . M
          */
         if (
-          safeGridGet(search, y - 1, x - 1) === 'S' &&
-          safeGridGet(search, y - 1, x + 1) === 'M' &&
-          safeGridGet(search, y + 1, x - 1) === 'S' &&
-          safeGridGet(search, y + 1, x + 1) === 'M'
+          safeGridGet(search, r - 1, c - 1) === 'S' &&
+          safeGridGet(search, r - 1, c + 1) === 'M' &&
+          safeGridGet(search, r + 1, c - 1) === 'S' &&
+          safeGridGet(search, r + 1, c + 1) === 'M'
         ) {
           count++
         }
@@ -140,10 +140,10 @@ export function solution2(input) {
          * M . S
          */
         if (
-          safeGridGet(search, y - 1, x - 1) === 'M' &&
-          safeGridGet(search, y - 1, x + 1) === 'S' &&
-          safeGridGet(search, y + 1, x - 1) === 'M' &&
-          safeGridGet(search, y + 1, x + 1) === 'S'
+          safeGridGet(search, r - 1, c - 1) === 'M' &&
+          safeGridGet(search, r - 1, c + 1) === 'S' &&
+          safeGridGet(search, r + 1, c - 1) === 'M' &&
+          safeGridGet(search, r + 1, c + 1) === 'S'
         ) {
           count++
         }
