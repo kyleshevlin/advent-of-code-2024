3126c5570a28239460ae3b1d3d1ef63228d7ac3a
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2024-12-08 09:15:16 -0800

    solutions for day 8

--- src/day-08/index.js
+++ src/day-08/index.js
@@ -1,9 +1,119 @@
-import { getInput } from '../utils'
+import { getInput, safeGridGet } from '../utils'
 
 const data = getInput(__dirname)
 
 const formatInput = input => input.trim().split('\n')
 
-export function solution1(input) {}
+function getNodes(grid) {
+  const result = []
 
-export function solution2(input) {}
+  for (let r = 0; r < grid.length; r++) {
+    for (let c = 0; c < grid[r].length; c++) {
+      const char = grid[r][c]
+
+      if (char !== '.') {
+        result.push({ char, r, c })
+      }
+    }
+  }
+
+  return result
+}
+
+export function getAntinodes(a, b) {
+  const { r: rA, c: cA } = a
+  const { r: rB, c: cB } = b
+
+  const rDiff = rA - rB
+  const cDiff = cA - cB
+
+  const result = [
+    { r: rA + rDiff, c: cA + cDiff },
+    { r: rA - rDiff * 2, c: cA - cDiff * 2 },
+  ]
+
+  return result
+}
+
+function getNodePairs(nodes) {
+  const result = []
+
+  for (let i = 0; i < nodes.length; i++) {
+    for (let j = i + 1; j < nodes.length; j++) {
+      result.push([nodes[i], nodes[j]])
+    }
+  }
+
+  return result
+}
+
+export function solution1(input, antinodeGetter = getAntinodes) {
+  const grid = formatInput(input)
+  const nodes = getNodes(grid)
+
+  const groups = {}
+  for (const node of nodes) {
+    const { char } = node
+
+    if (!groups[char]) {
+      groups[char] = []
+    }
+
+    groups[char].push(node)
+  }
+
+  const antinodeLocations = new Set()
+
+  for (const group of Object.values(groups)) {
+    if (group.length < 2) continue
+
+    for (const pair of getNodePairs(group)) {
+      for (const antinode of antinodeGetter(...pair, grid)) {
+        const { r, c } = antinode
+
+        if (safeGridGet(grid, r, c) !== undefined) {
+          antinodeLocations.add(`${r},${c}`)
+        }
+      }
+    }
+  }
+
+  return antinodeLocations.size
+}
+
+// console.log(solution1(data)) // 295
+
+function getAntinodesPart2(a, b, grid) {
+  const { r: rA, c: cA } = a
+  const { r: rB, c: cB } = b
+
+  const rDiff = rA - rB
+  const cDiff = cA - cB
+
+  const result = [a]
+  let dirty = true
+  let i = 0
+
+  while (dirty) {
+    dirty = false
+    i++
+
+    if (safeGridGet(grid, rA + i * rDiff, cA + i * cDiff)) {
+      result.push({ r: rA + i * rDiff, c: cA + i * cDiff })
+      dirty = true
+    }
+
+    if (safeGridGet(grid, rA - i * rDiff, cA - i * cDiff)) {
+      result.push({ r: rA - i * rDiff, c: cA - i * cDiff })
+      dirty = true
+    }
+  }
+
+  return result
+}
+
+export function solution2(input) {
+  return solution1(input, getAntinodesPart2)
+}
+
+// console.log(solution2(data)) // 1034
--- src/day-08/index.test.js
+++ src/day-08/index.test.js
@@ -1,11 +1,54 @@
-import { solution1, solution2 } from './'
+import { getAntinodes, solution1, solution2 } from './'
 
-const input = ``
+const input = `
+............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............
+`
 
 test('solution1', () => {
-  expect(solution1(input)).toEqual()
+  expect(solution1(input)).toEqual(14)
 })
 
 test('solution2', () => {
-  expect(solution2(input)).toEqual()
+  expect(solution2(input)).toEqual(34)
+})
+
+describe('getAntinodes', () => {
+  test('vertical', () => {
+    expect(getAntinodes({ r: 1, c: 1 }, { r: 2, c: 1 })).toEqual([
+      { r: 0, c: 1 },
+      { r: 3, c: 1 },
+    ])
+  })
+
+  test('horizontal', () => {
+    expect(getAntinodes({ r: 0, c: 1 }, { r: 0, c: 2 })).toEqual([
+      { r: 0, c: 0 },
+      { r: 0, c: 3 },
+    ])
+  })
+
+  test('diagonal down-right', () => {
+    expect(getAntinodes({ r: 1, c: 1 }, { r: 2, c: 2 })).toEqual([
+      { r: 0, c: 0 },
+      { r: 3, c: 3 },
+    ])
+  })
+
+  test('diagonal down-left', () => {
+    expect(getAntinodes({ r: 1, c: 2 }, { r: 2, c: 1 })).toEqual([
+      { r: 0, c: 3 },
+      { r: 3, c: 0 },
+    ])
+  })
 })
