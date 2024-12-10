ade7ed1b0a45a3351dda318fae00279e27a56007
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2024-12-10 11:57:47 -0800

    solutions for day 10

--- src/day-10/index.js
+++ src/day-10/index.js
@@ -1,9 +1,119 @@
-import { getInput } from '../utils'
+import { getInput, safeGridGet, sum } from '../utils'
 
 const data = getInput(__dirname)
 
-const formatInput = input => input.trim().split('\n')
+const formatInput = input =>
+  input
+    .trim()
+    .split('\n')
+    .map(line => line.split('').map(Number))
 
-export function solution1(input) {}
+function getTrailheads(grid) {
+  const result = []
 
-export function solution2(input) {}
+  for (let r = 0; r < grid.length; r++) {
+    for (let c = 0; c < grid[r].length; c++) {
+      if (grid[r][c] === 0) {
+        result.push([r, c])
+      }
+    }
+  }
+
+  return result
+}
+
+const NEXT_DIRS = [
+  [-1, 0],
+  [0, 1],
+  [1, 0],
+  [0, -1],
+]
+
+function getTrailScore(trailhead, grid) {
+  let score = 0
+  const visited = new Set()
+
+  function recurse(point, level) {
+    const key = point.join(',')
+
+    if (visited.has(key)) return
+
+    visited.add(key)
+
+    if (level === 9) {
+      score++
+      return
+    }
+
+    const [r, c] = point
+
+    for (const [y, x] of NEXT_DIRS) {
+      const nextRow = r + y
+      const nextCol = c + x
+
+      const nextLevel = safeGridGet(grid, nextRow, nextCol)
+
+      if (nextLevel === level + 1) {
+        recurse([nextRow, nextCol], nextLevel)
+      }
+    }
+  }
+
+  recurse(trailhead, 0)
+
+  return score
+}
+
+export function solution1(input) {
+  const grid = formatInput(input)
+  const trailheads = getTrailheads(grid)
+  const scores = trailheads.map(head => getTrailScore(head, grid))
+
+  return sum(scores)
+}
+
+// console.log(solution1(data)) // 459
+
+function getTrailRating(trailhead, grid) {
+  let score = 0
+  const visited = new Set()
+
+  function recurse(path, level) {
+    if (visited.has(path)) return
+
+    visited.add(path)
+
+    if (level === 9) {
+      score++
+      return
+    }
+
+    const point = path.split('-').at(-1).split(',').map(Number)
+    const [r, c] = point
+
+    for (const [y, x] of NEXT_DIRS) {
+      const nextRow = r + y
+      const nextCol = c + x
+
+      const nextLevel = safeGridGet(grid, nextRow, nextCol)
+
+      if (nextLevel === level + 1) {
+        recurse(path + `-${nextRow},${nextCol}`, nextLevel)
+      }
+    }
+  }
+
+  recurse(trailhead.join(','), 0)
+
+  return score
+}
+
+export function solution2(input) {
+  const grid = formatInput(input)
+  const trailheads = getTrailheads(grid)
+  const ratings = trailheads.map(head => getTrailRating(head, grid))
+
+  return sum(ratings)
+}
+
+// console.log(solution2(data)) // 1034
--- src/day-10/index.test.js
+++ src/day-10/index.test.js
@@ -1,11 +1,93 @@
 import { solution1, solution2 } from './'
 
-const input = ``
+const map1 = `
+0123
+1234
+8765
+9876
+`
+
+const map2 = `
+...0...
+...1...
+...2...
+6543456
+7.....7
+8.....8
+9.....9
+`
+
+const map3 = `
+..90..9
+...1.98
+...2..7
+6543456
+765.987
+876....
+987....
+`
+
+const map4 = `
+10..9..
+2...8..
+3...7..
+4567654
+...8..3
+...9..2
+.....01
+`
+
+const input = `
+89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732
+`
 
 test('solution1', () => {
-  expect(solution1(input)).toEqual()
+  expect(solution1(map1)).toEqual(1)
+  expect(solution1(map2)).toEqual(2)
+  expect(solution1(map3)).toEqual(4)
+  expect(solution1(map4)).toEqual(3)
+  expect(solution1(input)).toEqual(36)
 })
 
+const map5 = `
+.....0.
+..4321.
+..5..2.
+..6543.
+..7..4.
+..8765.
+..9....
+`
+
+const map6 = `
+..90..9
+...1.98
+...2..7
+6543456
+765.987
+876....
+987....
+`
+
+const map7 = `
+012345
+123456
+234567
+345678
+4.6789
+56789.
+`
+
 test('solution2', () => {
-  expect(solution2(input)).toEqual()
+  expect(solution2(map5)).toEqual(3)
+  expect(solution2(map6)).toEqual(13)
+  expect(solution2(map7)).toEqual(227)
+  expect(solution2(input)).toEqual(81)
 })
