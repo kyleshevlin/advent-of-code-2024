e9db65d2a2955652eab8e224f28796056c235f62
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2024-12-12 12:30:32 -0800

    day 12 solutions

--- src/day-12/index.js
+++ src/day-12/index.js
@@ -1,9 +1,162 @@
-import { getInput } from '../utils'
+import { getInput, safeGridGet } from '../utils'
 
 const data = getInput(__dirname)
 
 const formatInput = input => input.trim().split('\n')
 
-export function solution1(input) {}
+const NEXT_DIRS = [
+  [-1, 0],
+  [0, 1],
+  [1, 0],
+  [0, -1],
+]
 
-export function solution2(input) {}
+function getRegions(grid) {
+  const regions = []
+
+  const cellsRemaining = new Set()
+  for (let r = 0; r < grid.length; r++) {
+    for (let c = 0; c < grid[r].length; c++) {
+      cellsRemaining.add(`${r},${c}`)
+    }
+  }
+
+  function walk(row, col, char, positions) {
+    for (const [dy, dx] of NEXT_DIRS) {
+      const nextChar = safeGridGet(grid, row + dy, col + dx)
+      const nextRow = row + dy
+      const nextCol = col + dx
+      const key = `${nextRow},${nextCol}`
+
+      if (nextChar && nextChar === char && cellsRemaining.has(key)) {
+        positions.push([nextRow, nextCol])
+        cellsRemaining.delete(key)
+        walk(nextRow, nextCol, char, positions)
+      }
+    }
+  }
+
+  while (cellsRemaining.size) {
+    const cell = cellsRemaining.values().next().value
+    cellsRemaining.delete(cell)
+
+    const [row, col] = cell.split(',').map(Number)
+    const char = grid[row][col]
+    const positions = [[row, col]]
+
+    walk(row, col, char, positions)
+
+    regions.push({ char, positions })
+  }
+
+  return regions
+}
+
+function getArea(region) {
+  return region.positions.length
+}
+
+export function getPerimeter(region) {
+  const { positions } = region
+  const positionSet = new Set(positions.map(([r, c]) => `${r},${c}`))
+  let result = 0
+
+  for (const [row, col] of positions) {
+    let sides = 4
+
+    for (const [dy, dx] of NEXT_DIRS) {
+      const nextRow = row + dy
+      const nextCol = col + dx
+      const key = `${nextRow},${nextCol}`
+
+      if (positionSet.has(key)) sides--
+    }
+
+    result += sides
+  }
+
+  return result
+}
+
+export function solution1(input) {
+  const grid = formatInput(input)
+  const regions = getRegions(grid)
+
+  let result = 0
+  for (const region of regions) {
+    result += getArea(region) * getPerimeter(region)
+  }
+
+  return result
+}
+
+// console.log(solution1(data)) // 1533644
+
+export function getSides(region) {
+  const { positions } = region
+  const positionSet = new Set(positions.map(([r, c]) => `${r},${c}`))
+  let result = 0
+
+  for (const [row, col] of positions) {
+    let corners = 0
+
+    const n = `${row - 1},${col}`
+    const ne = `${row - 1},${col + 1}`
+    const e = `${row},${col + 1}`
+    const se = `${row + 1},${col + 1}`
+    const s = `${row + 1},${col}`
+    const sw = `${row + 1},${col - 1}`
+    const w = `${row},${col - 1}`
+    const nw = `${row - 1},${col - 1}`
+
+    // NE Corner
+    if (
+      (!positionSet.has(n) && !positionSet.has(e)) ||
+      (positionSet.has(n) && positionSet.has(e) && !positionSet.has(ne))
+    ) {
+      corners++
+    }
+
+    // SE Corner
+    if (
+      (!positionSet.has(s) && !positionSet.has(e)) ||
+      (positionSet.has(s) && positionSet.has(e) && !positionSet.has(se))
+    ) {
+      corners++
+    }
+
+    // SW Corner
+    if (
+      (!positionSet.has(s) && !positionSet.has(w)) ||
+      (positionSet.has(s) && positionSet.has(w) && !positionSet.has(sw))
+    ) {
+      corners++
+    }
+
+    // NW Corner
+    if (
+      (!positionSet.has(n) && !positionSet.has(w)) ||
+      (positionSet.has(n) && positionSet.has(w) && !positionSet.has(nw))
+    ) {
+      corners++
+    }
+
+    result += corners
+  }
+
+  return result
+}
+
+export function solution2(input) {
+  const grid = formatInput(input)
+  const regions = getRegions(grid)
+
+  let result = 0
+  for (const region of regions) {
+    result += getArea(region) * getSides(region)
+  }
+
+  return result
+}
+
+// console.log(solution2(data)) // 936718
--- src/day-12/index.test.js
+++ src/day-12/index.test.js
@@ -1,11 +1,108 @@
-import { solution1, solution2 } from './'
+import { getPerimeter, getSides, solution1, solution2 } from './'
 
-const input = ``
+const input = `
+RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE
+`
+
+const map1 = `
+AAAA
+BBCD
+BBCC
+EEEC
+`
+
+const map2 = `
+OOOOO
+OXOXO
+OOOOO
+OXOXO
+OOOOO
+`
 
 test('solution1', () => {
-  expect(solution1(input)).toEqual()
+  expect(solution1(map1)).toEqual(140)
+  expect(solution1(map2)).toEqual(772)
+  expect(solution1(input)).toEqual(1930)
 })
 
+const map3 = `
+EEEEE
+EXXXX
+EEEEE
+EXXXX
+EEEEE
+`
+
+const map4 = `
+AAAAAA
+AAABBA
+AAABBA
+ABBAAA
+ABBAAA
+AAAAAA
+`
+
 test('solution2', () => {
-  expect(solution2(input)).toEqual()
+  // expect(solution2(map1)).toEqual(80)
+  // expect(solution2(map2)).toEqual(436)
+  // expect(solution2(map3)).toEqual(236)
+  expect(solution2(map4)).toEqual(368)
+  // expect(solution2(input)).toEqual(1206)
+})
+
+test('getPerimeter', () => {
+  expect(getPerimeter({ positions: [[0, 0]] })).toEqual(4)
+  expect(
+    getPerimeter({
+      positions: [
+        [0, 0],
+        [0, 1],
+      ],
+    })
+  ).toEqual(6)
+  expect(
+    getPerimeter({
+      positions: [
+        [0, 0],
+        [0, 1],
+        [0, 2],
+        [1, 0],
+        [1, 2],
+        [2, 0],
+        [2, 1],
+        [2, 2],
+      ],
+    })
+  ).toEqual(16)
+})
+
+test('getSides', () => {
+  expect(getSides({ positions: [[0, 0]] })).toEqual(4)
+  expect(
+    getSides({
+      positions: [
+        [0, 0],
+        [0, 1],
+      ],
+    })
+  ).toEqual(4)
+  expect(
+    getSides({
+      positions: [
+        [0, 0],
+        [1, 0],
+        [1, 1],
+        [2, 1],
+      ],
+    })
+  ).toEqual(8)
 })
