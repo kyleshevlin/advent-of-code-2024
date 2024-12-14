4e412e47dd51294deaf862d01ddc580c2f2f9687
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2024-12-14 09:30:30 -0800

    day 14 solutions

--- src/day-14/index.js
+++ src/day-14/index.js
@@ -2,8 +2,109 @@ import { getInput } from '../utils'
 
 const data = getInput(__dirname)
 
-const formatInput = input => input.trim().split('\n')
+const formatInput = input => input.trim().split('\n').map(makeRobot)
 
-export function solution1(input) {}
+function makeRobot(robotStr) {
+  let [p, v] = robotStr.split(' ')
+  p = p.replace('p=', '')
+  v = v.replace('v=', '')
 
-export function solution2(input) {}
+  const [px, py] = p.split(',').map(Number)
+  const [vx, vy] = v.split(',').map(Number)
+
+  return { p: { x: px, y: py }, v: { x: vx, y: vy } }
+}
+
+export function advanceRobot(robot, rows, cols) {
+  const { p, v } = robot
+
+  let nextPx = p.x + v.x
+
+  if (nextPx < 0 || nextPx >= cols) {
+    // adding cols should handle negative numbers
+    nextPx = (nextPx + cols) % cols
+  }
+
+  let nextPy = p.y + v.y
+
+  if (nextPy < 0 || nextPy >= rows) {
+    nextPy = (nextPy + rows) % rows
+  }
+
+  return { p: { x: nextPx, y: nextPy }, v }
+}
+
+export function solution1(input, rows, cols) {
+  const robots = formatInput(input)
+
+  let i = 0
+  let statefulBots = [...robots]
+  while (i < 100) {
+    statefulBots = statefulBots.map(robot => advanceRobot(robot, rows, cols))
+    i++
+  }
+
+  const xMid = Math.floor(cols / 2)
+  const yMid = Math.floor(rows / 2)
+
+  const nw = []
+  const ne = []
+  const sw = []
+  const se = []
+
+  for (const bot of statefulBots) {
+    if (bot.p.x < xMid && bot.p.y < yMid) {
+      nw.push(bot)
+      continue
+    }
+
+    if (bot.p.x > xMid && bot.p.y < yMid) {
+      ne.push(bot)
+      continue
+    }
+
+    if (bot.p.x < xMid && bot.p.y > yMid) {
+      sw.push(bot)
+      continue
+    }
+
+    if (bot.p.x > xMid && bot.p.y > yMid) {
+      se.push(bot)
+      continue
+    }
+  }
+
+  return nw.length * ne.length * sw.length * se.length
+}
+
+// console.log(solution1(data, 103, 101)) // 221616000
+
+/**
+ * I won't even lie, this solution feels like magic to me. The fact that when
+ * none of the bots overlap they make a Christmas tree is non-sense to me, but
+ * it's how others were figuring it out on Reddit.
+ *
+ * Personally, I think they should have given us an ASCII of the tree and just
+ * made us check against that.
+ */
+export function solution2(input, rows, cols) {
+  const robots = formatInput(input)
+
+  let i = 0
+  let statefulBots = [...robots]
+  let positionSet
+  let done = false
+
+  do {
+    i++
+    statefulBots = statefulBots.map(robot => advanceRobot(robot, rows, cols))
+    positionSet = new Set(statefulBots.map(bot => `${bot.p.x},${bot.p.y}`))
+
+    // No overlap would mean the sizes are the same
+    done = positionSet.size === robots.length
+  } while (!done)
+
+  return i
+}
+
+// console.log(solution2(data, 103, 101)) // 7572
--- src/day-14/index.test.js
+++ src/day-14/index.test.js
@@ -1,11 +1,39 @@
-import { solution1, solution2 } from './'
+import { advanceRobot, solution1 } from './'
 
-const input = ``
+const input = `
+p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3
+`
 
 test('solution1', () => {
-  expect(solution1(input)).toEqual()
+  expect(solution1(input, 7, 11)).toEqual(12)
 })
 
-test('solution2', () => {
-  expect(solution2(input)).toEqual()
+function makeBot(px, py, vx, vy) {
+  return { p: { x: px, y: py }, v: { x: vx, y: vy } }
+}
+
+test('advanceRobot', () => {
+  expect(advanceRobot(makeBot(0, 0, 1, 0), 10, 10)).toEqual(makeBot(1, 0, 1, 0))
+  expect(advanceRobot(makeBot(0, 0, 0, 1), 10, 10)).toEqual(makeBot(0, 1, 0, 1))
+  expect(advanceRobot(makeBot(1, 1, -1, -1), 10, 10)).toEqual(
+    makeBot(0, 0, -1, -1)
+  )
+
+  // wraps around x positively
+  expect(advanceRobot(makeBot(1, 0, 2, 0), 3, 3)).toEqual(makeBot(0, 0, 2, 0))
+  expect(advanceRobot(makeBot(2, 0, 4, 0), 5, 5)).toEqual(makeBot(1, 0, 4, 0))
+
+  // wraps around x negatively
+  expect(advanceRobot(makeBot(1, 0, -2, 0), 3, 3)).toEqual(makeBot(2, 0, -2, 0))
 })
