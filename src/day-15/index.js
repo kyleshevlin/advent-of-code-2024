bcc8faf8e7076f2e1dc2fdf49f7516f6c94ddc57
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2024-12-15 17:32:03 -0800

    day 15 pt 1

--- src/day-15/index.js
+++ src/day-15/index.js
@@ -1,9 +1,94 @@
-import { getInput } from '../utils'
+import { getInput, safeGridGet, sum } from '../utils'
 
 const data = getInput(__dirname)
 
-const formatInput = input => input.trim().split('\n')
+const EMPTY = '.'
+const WALL = '#'
+const BOX = 'O'
+const BOT = '@'
 
-export function solution1(input) {}
+const MOVE_TO_COORDS = {
+  '<': [0, -1],
+  '>': [0, 1],
+  '^': [-1, 0],
+  v: [1, 0],
+}
+
+const formatInput = input => {
+  const [gridLines, moveLines] = input.trim().split('\n\n')
+
+  const grid = gridLines.split('\n').map(line => line.split(''))
+  const moves = moveLines.split('\n').join('')
+
+  return { grid, moves }
+}
+
+function canBoxMove(grid, row, col, dRow, dCol, depth = 1) {
+  const nextRow = row + dRow
+  const nextCol = col + dCol
+  const nextChar = safeGridGet(grid, nextRow, nextCol)
+
+  switch (nextChar) {
+    case EMPTY:
+      return depth
+    case WALL:
+      return 0
+    case BOX:
+      return canBoxMove(grid, nextRow, nextCol, dRow, dCol, depth + 1)
+  }
+}
+
+export function solution1(input) {
+  const { grid, moves } = formatInput(input)
+
+  for (let i = 0; i < moves.length; i++) {
+    const move = moves[i]
+    const [dRow, dCol] = MOVE_TO_COORDS[move]
+
+    const botRow = grid.findIndex(row => row.includes(BOT))
+    const botCol = grid[botRow].indexOf(BOT)
+
+    const nextRow = botRow + dRow
+    const nextCol = botCol + dCol
+
+    const nextChar = safeGridGet(grid, nextRow, nextCol)
+
+    if (nextChar === WALL) {
+      continue
+    }
+
+    if (nextChar === EMPTY) {
+      grid[botRow][botCol] = EMPTY
+      grid[nextRow][nextCol] = BOT
+      continue
+    }
+
+    if (nextChar === BOX) {
+      const canMoveInXSpaces = canBoxMove(grid, nextRow, nextCol, dRow, dCol)
+
+      if (!canMoveInXSpaces) continue
+
+      const swapRow = dRow * canMoveInXSpaces
+      const swapCol = dCol * canMoveInXSpaces
+
+      grid[nextRow + swapRow][nextCol + swapCol] = BOX
+      grid[nextRow][nextCol] = BOT
+      grid[botRow][botCol] = EMPTY
+    }
+  }
+
+  const scores = []
+  for (let r = 0; r < grid.length; r++) {
+    for (let c = 0; c < grid[r].length; c++) {
+      if (grid[r][c] === BOX) {
+        scores.push(r * 100 + c)
+      }
+    }
+  }
+
+  return sum(scores)
+}
+
+// console.log(solution1(data)) // 1511865
 
 export function solution2(input) {}
--- src/day-15/index.test.js
+++ src/day-15/index.test.js
@@ -1,9 +1,45 @@
 import { solution1, solution2 } from './'
 
-const input = ``
+const inputSmall = `
+########
+#..O.O.#
+##@.O..#
+#...O..#
+#.#.O..#
+#...O..#
+#......#
+########
+
+<^^>>>vv<v>>v<<
+`
+
+const input = `
+##########
+#..O..O.O#
+#......O.#
+#.OO..O.O#
+#..O@..O.#
+#O#..O...#
+#O..O..O.#
+#.OO.O.OO#
+#....O...#
+##########
+
+<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
+`
 
 test('solution1', () => {
-  expect(solution1(input)).toEqual()
+  // expect(solution1(inputSmall)).toEqual(2028)
+  expect(solution1(input)).toEqual(10092)
 })
 
 test('solution2', () => {
