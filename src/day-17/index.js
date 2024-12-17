7b705e9fa79213074515db452cfd5622f152a361
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2024-12-17 09:40:00 -0800

    day 17 wip

--- src/day-17/index.js
+++ src/day-17/index.js
@@ -2,8 +2,142 @@ import { getInput } from '../utils'
 
 const data = getInput(__dirname)
 
-const formatInput = input => input.trim().split('\n')
+const formatInput = input => {
+  const [registerLines, programLine] = input.trim().split('\n\n')
 
-export function solution1(input) {}
+  const [registerA, registerB, registerC] = registerLines
+    .split('\n')
+    .map(line => Number(line.split(':').at(1).trim()))
 
-export function solution2(input) {}
+  const program = programLine.split(':').at(1).trim().split(',').map(Number)
+
+  return {
+    registerA,
+    registerB,
+    registerC,
+    program,
+  }
+}
+
+function executeProgram(a, b, c, program) {
+  let pointer = 0
+  const output = []
+
+  function getComboOperand(operand) {
+    switch (operand) {
+      case 0:
+      case 1:
+      case 2:
+      case 3:
+        return operand
+
+      case 4:
+        return a
+
+      case 5:
+        return b
+
+      case 6:
+        return c
+        l
+    }
+  }
+
+  while (pointer < program.length) {
+    const opcode = program[pointer]
+    const operand = program[pointer + 1]
+
+    switch (opcode) {
+      // adv
+      case 0: {
+        const numerator = a
+        const denominator = 2 ** getComboOperand(operand)
+
+        a = Math.trunc(numerator / denominator)
+        break
+      }
+
+      // bxl
+      case 1: {
+        b = b ^ operand
+        break
+      }
+
+      // bst
+      case 2: {
+        b = getComboOperand(operand) % 8
+        break
+      }
+
+      // jnz
+      case 3: {
+        if (a === 0) break
+
+        pointer = operand
+        continue
+      }
+
+      // bxc
+      case 4: {
+        b = b ^ c
+        break
+      }
+
+      // out
+      case 5: {
+        const result = getComboOperand(operand) % 8
+        output.push(result)
+        break
+      }
+
+      // bdv
+      case 6: {
+        const numerator = a
+        const denominator = 2 ** getComboOperand(operand)
+
+        b = Math.trunc(numerator / denominator)
+        break
+      }
+
+      // cdv
+      case 7: {
+        const numerator = a
+        const denominator = 2 ** getComboOperand(operand)
+
+        c = Math.trunc(numerator / denominator)
+        break
+      }
+    }
+
+    pointer += 2
+  }
+
+  return output.join(',')
+}
+
+export function solution1(input) {
+  const { registerA, registerB, registerC, program } = formatInput(input)
+
+  return executeProgram(registerA, registerB, registerC, program)
+}
+
+// console.log(solution1(data)) // 3,6,7,0,5,7,3,1,4
+
+export function solution2(input) {
+  const { registerB, registerC, program } = formatInput(input)
+  const expected = program.join(',')
+
+  let registerA = 0
+  // eslint-disable-next-line
+  while (true) {
+    const result = executeProgram(registerA, registerB, registerC, program)
+
+    if (result === expected) break
+
+    registerA++
+  }
+
+  return registerA
+}
+
+// console.log(solution2(data))
--- src/day-17/index.test.js
+++ src/day-17/index.test.js
@@ -1,11 +1,25 @@
 import { solution1, solution2 } from './'
 
-const input = ``
+const input = `
+Register A: 729
+Register B: 0
+Register C: 0
+
+Program: 0,1,5,4,3,0
+`
 
 test('solution1', () => {
-  expect(solution1(input)).toEqual()
+  expect(solution1(input)).toEqual('4,6,3,5,6,3,5,2,1,0')
 })
 
+const input2 = `
+Register A: 2024
+Register B: 0
+Register C: 0
+
+Program: 0,3,5,4,3,0
+`
+
 test('solution2', () => {
-  expect(solution2(input)).toEqual()
+  expect(solution2(input2)).toEqual(117440)
 })
