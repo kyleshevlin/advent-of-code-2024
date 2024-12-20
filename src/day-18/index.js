938d325c512ac6ffa77573fc207db5e2a1132f91
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2024-12-19 19:46:23 -0800

    day 18 solutions

--- src/day-18/index.js
+++ src/day-18/index.js
@@ -1,9 +1,122 @@
-import { getInput } from '../utils'
+import { getInput, safeGridGet } from '../utils'
 
 const data = getInput(__dirname)
 
-const formatInput = input => input.trim().split('\n')
+const formatInput = input =>
+  input
+    .trim()
+    .split('\n')
+    .map(line => line.split(',').map(Number))
 
-export function solution1(input) {}
+const NEXT_DIRS = [
+  [-1, 0],
+  [0, 1],
+  [1, 0],
+  [0, -1],
+]
 
-export function solution2(input) {}
+function makeKey(cell) {
+  return cell.join(',')
+}
+
+export function solution1(input, gridSize, limit) {
+  const bytes = formatInput(input)
+  const grid = Array.from({ length: gridSize + 1 }).map(() =>
+    Array.from({ length: gridSize + 1 }).fill('.')
+  )
+
+  for (let i = 0; i < limit; i++) {
+    const [x, y] = bytes[i]
+
+    grid[y][x] = '#'
+  }
+
+  const start = [0, 0]
+  const end = [gridSize, gridSize]
+
+  const queue = [{ cell: start, path: [] }]
+  const visited = new Set()
+  visited.add(makeKey(start))
+
+  let result
+  while (queue.length) {
+    const item = queue.shift()
+    const { cell, path } = item
+    const [row, col] = cell
+
+    if (row === end[0] && col === end[1]) {
+      result = item
+      break
+    }
+
+    for (const [dy, dx] of NEXT_DIRS) {
+      const newRow = row + dy
+      const newCol = col + dx
+      const key = makeKey([newRow, newCol])
+      const newCell = safeGridGet(grid, newRow, newCol)
+
+      if (newCell === '.' && !visited.has(key)) {
+        visited.add(key)
+        queue.push({ cell: [newRow, newCol], path: [...path, newCell] })
+      }
+    }
+  }
+
+  const { path } = result
+
+  return path.length
+}
+
+// console.log(solution1(data, 70, 1024)) // 276
+
+export function solution2(input, gridSize) {
+  const bytes = formatInput(input)
+  const grid = Array.from({ length: gridSize + 1 }).map(() =>
+    Array.from({ length: gridSize + 1 }).fill('.')
+  )
+
+  let canEscape = true
+  let result
+  outer: while (canEscape) {
+    canEscape = false
+    const byte = bytes.shift()
+    const [row, col] = byte
+    grid[row][col] = '#'
+
+    const start = [0, 0]
+    const end = [gridSize, gridSize]
+
+    const queue = [{ cell: start, path: [] }]
+    const visited = new Set()
+    visited.add(makeKey(start))
+
+    while (queue.length) {
+      const item = queue.shift()
+      const { cell, path } = item
+      const [row, col] = cell
+
+      if (row === end[0] && col === end[1]) {
+        canEscape = true
+        continue outer
+      }
+
+      for (const [dy, dx] of NEXT_DIRS) {
+        const newRow = row + dy
+        const newCol = col + dx
+        const key = makeKey([newRow, newCol])
+        const newCell = safeGridGet(grid, newRow, newCol)
+
+        if (newCell === '.' && !visited.has(key)) {
+          visited.add(key)
+          queue.push({ cell: [newRow, newCol], path: [...path, newCell] })
+        }
+      }
+    }
+
+    result = byte
+  }
+
+  return result.join(',')
+}
+
+// console.log(solution2(data, 70)) // 60,37
--- src/day-18/index.test.js
+++ src/day-18/index.test.js
@@ -1,11 +1,37 @@
 import { solution1, solution2 } from './'
 
-const input = ``
+const input = `
+5,4
+4,2
+4,5
+3,0
+2,1
+6,3
+2,4
+1,5
+0,6
+3,3
+2,6
+5,1
+1,2
+5,5
+2,5
+6,5
+1,4
+0,4
+6,4
+1,1
+6,1
+1,0
+0,5
+1,6
+2,0
+`
 
 test('solution1', () => {
-  expect(solution1(input)).toEqual()
+  expect(solution1(input, 6, 12)).toEqual(22)
 })
 
 test('solution2', () => {
-  expect(solution2(input)).toEqual()
+  expect(solution2(input, 6)).toEqual('6,1')
 })
